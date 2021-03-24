718d6fd44eaf3137dee4037c6d02ac4b81257ba0
Jake <jayeshkvarma@gmail.com>
2021-03-24 21:59:30 +0530

    Initial App commit

---
+++ app/src/main/java/com/jkv/myapplication/CheckJGit.java
@@ -0,0 +1,11 @@
+package main.java.com.jkv.myapplication;
+
+public class CheckJGit {
+
+    public static void main(String[] args){
+
+        System.out.println("Inside Check JGit.");
+
+
+    }
+}
